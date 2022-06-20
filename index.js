3b15582ea007bbfef349aef375bcddcd9d169abe
ismail-hossen <satict2016@gmail.com>
2022-06-20 16:09:58 +0600

    basic setup with controller and router and index ejs added

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+// external imports
 const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+
+// internal imports
+const {
+  errorHandler,
+  notFoundHandler,
+} = require("./middleware/common/errorHandler");
+const loginRouter = require("./router/loginRouter");
 
 const app = express();
 dotenv.config();
@@ -29,7 +37,14 @@ app.use(express.static(path.join(__dirname, "public")));
 // cookie parsers
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
+// routing setup
+app.get("/", loginRouter);
+
 // 404 not found error handling
+app.use(notFoundHandler);
+
+// common error handler
+app.use(errorHandler);
 
 // app listener
 app.listen(process.env.PORT, () =>
