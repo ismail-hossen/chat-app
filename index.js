a343833d7ee8d8cd069ab2a38499bdae37d0425f
ismail-hossen <satict2016@gmail.com>
2022-06-20 09:23:02 +0600

    project basic setup completed

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const path = require("path");
 const mongoose = require("mongoose");
+const { urlencoded } = require("express");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 dotenv.config();
@@ -13,3 +16,21 @@ mongoose
   })
   .then(() => console.log("database connected"))
   .catch((err) => console.log(err));
+
+//   request parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// set view engine
+app.set("view engine", "ejs");
+
+// set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// cookie parsers
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+// app listener
+app.listen(process.env.PORT, () =>
+  console.log(`app listening to port ${process.env.PORT}`)
+);
