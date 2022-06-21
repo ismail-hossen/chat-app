2efa3befc9b703982c8a5ed6a7baaecc55515145
ismail-hossen <satict2016@gmail.com>
2022-06-21 12:30:19 +0600

    implemented a middleware with multer for file upload and same as users validators middleware added

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -4,10 +4,15 @@ const express = require("express");
 // internal imports
 const { getUsers } = require("../controller/usersController");
 const decorateHtmlResponse = require("../middleware/common/decorateHtmlResponse");
+const { addUserValidators } = require("../middleware/users/userValidators");
+const avatarUpload = require("../middleware/users/avatarUpload");
 
 const router = express.Router();
 
 // users page
 router.get("/users", decorateHtmlResponse("Users"), getUsers);
+
+// add user
+router.post("/", avatarUpload, addUserValidators);
 
 module.exports = router;
